f111a4d70db16855e49468a245e3cb62928d66b3
burdbrains <burdwilliamethan@gmail.com>
2023-02-04 21:59:59 -0600

    Fixed Gliding velocity. Started fall damage cancel.

--- src/main/java/com/ethan/messaround/ChickenFlyingEvents.java
+++ src/main/java/com/ethan/messaround/ChickenFlyingEvents.java
@@ -1,21 +1,34 @@
 package com.ethan.messaround;
 
-import jdk.javadoc.internal.doclint.HtmlTag;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
 
 public class ChickenFlyingEvents implements Listener {
 
+
+    @EventHandler
+    public void onFallDamage(EntityDamageEvent e){
+        EntityDamageEvent.DamageCause dCause = e.getCause();
+
+        boolean dmgByFall = dCause == EntityDamageEvent.DamageCause.FALL;
+
+
+    }
+
     @EventHandler
     public void onPlayerFalling(PlayerMoveEvent e){
         Player p = e.getPlayer();
         Location from = e.getFrom();
+
+        // get block below the player
+        from.setY(from.getY()-1);
 
         boolean hasPassengers = p.getPassengers().size() > 0;
         if (hasPassengers){
@@ -26,7 +39,9 @@ public class ChickenFlyingEvents implements Listener {
 
                 // Change player velocity
                 // to fall slower
-                pVel.setY(pVel.getY() * 0.9);
+                pVel.setY(pVel.getY() * 0.8);
+                pVel.setX(pVel.getX() * 1.1);
+                pVel.setZ(pVel.getZ() * 1.1);
                 p.setVelocity(pVel);
 
                 // Add code to check for
