799aac086295dd8e70e3c94a8faf6142397f1853
burdbrains <burdwilliamethan@gmail.com>
2023-02-06 21:49:35 -0600

    Gliding velocity is now constant and stored in a Hashmap.
    Working on changing players x & z direction in accordance to their movement input, but still maintaining their constant gliding velocity.

--- src/main/java/com/ethan/messaround/ChickenFlyingEvents.java
+++ src/main/java/com/ethan/messaround/ChickenFlyingEvents.java
@@ -11,11 +11,13 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
 
-import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class ChickenFlyingEvents implements Listener {
 
-    Dictionary<Player, Vector> chickenVectors;
+    Map<Player, Vector> chickenVectors = new HashMap<>();
 
     @EventHandler
     public void onFallDamage(EntityDamageEvent e){
@@ -32,6 +34,12 @@ public class ChickenFlyingEvents implements Listener {
                 boolean passengerIsChicken = p.getPassengers().get(0).getType() == EntityType.CHICKEN;
                 if(passengerIsChicken && dmgByFall){
                     e.setCancelled(true);
+
+                    boolean playerHasKey = chickenVectors.get(p) != null;
+
+                    if (playerHasKey) {
+                        chickenVectors.remove(p);
+                    }
                 }
             }
         }
@@ -43,7 +51,11 @@ public class ChickenFlyingEvents implements Listener {
     @EventHandler
     public void onPlayerFalling(PlayerMoveEvent e){
         Player p = e.getPlayer();
+
         Location from = e.getFrom();
+        Location to = e.getTo();
+
+        p.sendMessage(ChatColor.AQUA + "X: " + p.getVelocity().getX() + " /  Z: " + p.getVelocity().getZ());
 
         // get block below the player
         from.setY(from.getY()-1);
@@ -82,22 +94,67 @@ public class ChickenFlyingEvents implements Listener {
                 //              * so falling is constant
                 //              * w/ chicken and air block
 
+                boolean playerHasKey = chickenVectors.get(p) != null;
+
                 p.sendMessage(ChatColor.GREEN + "yVel=" + yVel);
                 if (blockBelowAir && hasChickenPassenger && reachedPeakY) {
+
                     p.sendMessage(ChatColor.RED + "FIRED... yVel=" + yVel);
 
                     Vector pVel = p.getVelocity();
+                    double pY = pVel.getY();
+                    double pX = pVel.getX();
+                    double pZ = pVel.getZ();
 
                     // Change player velocity
                     // to fall slower
-                    pVel.setY(pVel.getY() * yMultiplier);
-                    pVel.setX(pVel.getX() * xzMultiplier);
-                    pVel.setZ(pVel.getZ() * xzMultiplier);
+                    pVel.setY(pY * yMultiplier);
+                    pVel.setX(pX * xzMultiplier);
+                    pVel.setZ(pZ * xzMultiplier);
+
+                    chickenVectors.put(p, pVel);
+
                     p.setVelocity(pVel);
 
                     // Add code to check for
                     // jumping vs falling
                     // (right now w/ chicken you'll jump 0.25 height)
+                }
+                else if (blockBelowAir && hasChickenPassenger && playerHasKey)
+                {
+                    // uses chicken vector when gliding
+                    // and updates xz vector to players inputs
+                    Vector playerChickenVector = chickenVectors.get(p);
+
+                    // Gets the sign of the players attempted vectors
+                    // and applies that to gliding vectors
+                    // which changes vector direction according
+                    // to player moves
+                    double differenceX = to.getX() - from.getX();
+                    double differenceZ = to.getZ() - from.getZ();
+
+                    double updatedVecX = Math.signum(differenceX);
+                    double updatedVecZ = Math.signum(differenceZ);
+                    // VECTORS DON'T CONTAIN THE PLAYERS DIRECTION
+                    // THAT WILL HAVE TO BE DETERMINED WITH FROM & TO LOCATION
+
+                    // direction doesn't work because if ur going in neg direction
+                    // and try to move in that direction -1 * -1 = 1
+                    // so you start moving in the opposite direction
+                    // we need to just set new x & z velocity to the signs
+                    // we retrieve, can create util function for this
+
+
+
+                    p.sendMessage(ChatColor.YELLOW + "X sign: " + updatedVecX);
+                    p.sendMessage(ChatColor.GOLD + "Z sign: " + updatedVecZ);
+
+                    playerChickenVector.setX(playerChickenVector.getX()*updatedVecX);
+                    playerChickenVector.setZ(playerChickenVector.getZ()*updatedVecZ);
+
+                    p.setVelocity(playerChickenVector);
+
+                    chickenVectors.put(p, playerChickenVector);
                 }
             }
         }
